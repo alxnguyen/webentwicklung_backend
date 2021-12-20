425e34844cff936f2573d2e36fab36d37bfb4375
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-20 23:43:34 +0100

    sadsadsadsa

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const Users = require("./models/dbHelpers");
 var app=express();
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.render("index.html");
+app.use(express.urlencoded({extended: false}));
+
+app.get("/", async (req, res) => {
+    const user = Users.findUserByEmail(req.body.email);
+    if(!user) {
+        return res.status(400).send("Cannot find user");
+    } else {
+        return res.status(201).send("User found");
+    }
 });
 
 app.listen(process.env.PORT ||3000);

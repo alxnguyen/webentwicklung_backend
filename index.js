5067e353d7039351ee6c863be28d8a72401e68bc
alxnguyen <alexander.nguyen1509@gmail.com>
2021-12-21 18:46:23 +0100

    updated login function

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var express=require('express');
 var cors=require('cors');
 const Users = require("./models/dbHelpers");
+const bp = require('body-parser');
 
 var app=express();
 app.use(cors());
 app.use(express.json());
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
-app.use(express.urlencoded({extended: false}));
-
-app.get("/", (req, res) => {
-    Users.findAllUsers()
-      .then((users) => {
-        res.status(200).json(users);
-      })
-      .catch((error) => {
-        res.status(500).json({ message: "unable to retrieve users" });
-      });
-  });
-
-app.post("/", async (req, res) => {
-    const user = await Users.findUserByEmail(req.body.email);
-    if(!user) {
-        return res.status(400).send("Cannot find user");
-    } else {
-        return res.status(201).send("User found");
-    }
+app.post("/", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email);
+    console.log(password);
+    const user = Users.findUserByEmail(req.body.email).then((user) => {
+        console.log(user);
+        if(!user) {
+            return res.status(400).send("Cannot find user");
+        } else {
+            if(password === user.password) {
+              return res.status(201).send("User found");
+            } else {
+              return res.status(401).send("Wrong password");
+            }
+        }
+    }); 
 });
 
 app.listen(process.env.PORT ||3000);

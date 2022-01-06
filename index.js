393ca524c638222553282a4e361bdaaa9c92ea16
soosaasees <nachokaese@gmx.net>
2022-01-06 15:14:22 +0100

    bugfixing (?/?)

--- index.js
+++ index.js
@@ -11,44 +11,43 @@ app.use(bp.urlencoded({ extended: true }))
 
 
 app.post("/", async (req, res) => {
-    async function checkPassword(email, password)   {
-      const user= Users.findUserByEmail(email).then((user) => {
-          if(!user)   {
-              console.log("user existiert nicht");
-              return false;
-         } else  {
-             if(password==user.password) {
-                console.log("user und passwort existieren");
-                return true;
-              } else  {
-                console.log("User existiert, passwort nicht");
-                return false;
-             }
-         }
-      });
+  async function checkPassword(email, password)   {
+    const user= Users.findUserByEmail(email).then((user) => {
+      if(!user)   {
+        console.log("user existiert nicht");
+        return false;
+      } else  {
+          if(password==user.password) {
+            console.log("user und passwort existieren");
+            return true;
+          } else  {
+            console.log("User existiert, passwort nicht");
+            return false;
+          }
+      }
+    });
   }
 
   async function login(email, password)    {
-     const correctPassword=await checkPassword(email, password).then((correctPassword)=>  {
-      if(correctPassword) {
-        console.log("erstelle sessionId");
-        const sessionId=crypto.randomUUID();
-       return sessionId;
+    correctPassword=await checkPassword(email, password);
+    if(correctPassword) {
+      console.log("erstelle sessionId");
+      const sessionId=crypto.randomUUID();
+      return sessionId;
+    } else  {
+      console.log("wtf");
+      return undefined
     }
-    console.log("wtf");
-   return undefined
-     });
   }
 
   const email = req.body.email;
   const password = req.body.password;
-  const sessionId = await login(email, password).then((sessionId)=> {
-    if(!sessionId)  {
-      return res.status(400).send("User Authentification failed");
-    } else  {
-      return res.status(201).send("User found");
-    }
-  });
+  sessionId=await login(email, password)
+  if(!sessionId)  {
+    return res.status(400).send("User Authentification failed");
+  } else  {
+    return res.status(201).send("User found");
+  }
 });
 
 app.listen(process.env.PORT ||3000);

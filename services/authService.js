b27da7b35af724fcbb062921544f65ff4174a250
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-07 00:06:52 +0100

    removed console logs

--- services/authService.js
+++ services/authService.js
@@ -32,12 +32,8 @@ client.on("connect", () => console.log("Successfully connected to redis"));
       res=await bcrypt.compare(password, user.password);
       console.log(res);
         if(res) {
-          passwordHash=await bcrypt.hash(password, 10);
-          await console.log("funktioniert eig. alles: "+passwordHash );
           return true;
         } else {
-          passwordString="falsches Passwort: "+password+ "   " + await bcrypt.hash(password, 10)+ "    " + user.password;
-          console.log(passwordString);
           return false;
         }
     }
@@ -46,14 +42,11 @@ client.on("connect", () => console.log("Successfully connected to redis"));
 
   async function login(email, password)    {
     correctPassword=await checkPassword(email, password);
-    console.log("hallohallo:  "+ correctPassword)
     if(correctPassword) {
-      console.log("hallohallohallo");
       const sessionId=crypto.randomUUID();
       await client.set(sessionId, email, { EX: 60 });
       return sessionId;
     } else  {
-      console.log("tschuess  "+ correctPassword)
       return undefined;
     }
   }

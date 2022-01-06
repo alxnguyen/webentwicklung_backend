86278bde9d3e74e06fceba6edf6d2ddd8a4b5184
soosaasees <nachokaese@gmx.net>
2022-01-06 13:28:26 +0100

    bugfixing 2/?

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ app.use(bp.urlencoded({ extended: true }))
 app.post("/", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  const sessionId = await authService.login(email, password);
-  if(!sessionId)  {
-    return res.status(400).send("User Authentification failed");
-  } else  {
-    return res.status(201).send("User found");
-  }
+  const sessionId = await authService.login(email, password).then((sessionId)=> {
+    if(!sessionId)  {
+      return res.status(400).send("User Authentification failed");
+    } else  {
+      return res.status(201).send("User found");
+    }
+  });
 });
 
 app.listen(process.env.PORT ||3000);

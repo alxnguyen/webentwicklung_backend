dc9080e471a34b75e6c8af8716f76d3264dac562
soosaasees <nachokaese@gmx.net>
2022-01-06 14:25:25 +0100

    bugfixing (13/?)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express=require('express');
 var cors=require('cors');
 const bp = require('body-parser');
-const authService=require("./services/authService");
 
 var app=express();
 app.use(cors());
@@ -9,13 +8,38 @@ app.use(express.json());
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
+
+
 app.post("/", async (req, res) => {
+    async function checkPassword(email, password)   {
+      const user= Users.findUserByEmail(email).then((user) => {
+          if(!user)   {
+              return false;
+         } else  {
+             if(password==user.password) {
+                  return true;
+              } else  {
+                  return false;
+             }
+         }
+      });
+  }
+
+  async function login(email, password)    {
+     const correctPassword=await this.checkPassword(email, password);
+      if(correctPassword) {
+          const sessionId=crypto.randomUUID();
+         return sessionId;
+      }
+     return undefined
+  }
+
   const email = req.body.email;
   const password = req.body.password;
   //bugfixing
   console.log(authService)
   //bugfixing
-  const sessionId = await authService.login(email, password).then((sessionId)=> {
+  const sessionId = await this.login(email, password).then((sessionId)=> {
     if(!sessionId)  {
       return res.status(400).send("User Authentification failed");
     } else  {

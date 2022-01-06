0efba833cb1704a884bffa4b547f35e24cc1cd24
soosaasees <nachokaese@gmx.net>
2022-01-06 17:22:56 +0100

    logging entfernt. redis connected jetzt

--- services/authService.js
+++ services/authService.js
@@ -11,7 +11,6 @@ module.exports = {
 const client = redis.createClient({
     url:process.env.REDIS_URL
 });
-console.log(process.env.REDIS_URL);
 client.on("error", (err) => console.log("Redis Client Error", err));
 client.on("connect", () => console.log("Successfully connected to redis"));
 

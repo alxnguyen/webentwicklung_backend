473e2262314f5a7875c24a36a2fd2c1e32e55aff
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-06 21:45:17 +0100

    added more console logs

--- seeds/create_test_users.js
+++ seeds/create_test_users.js
@@ -8,13 +8,16 @@ exports.seed = function(knex) {
       const saltRounds = 10;
 
       var password1 = bcrypt.hash("password1", saltRounds, function(err, hash) {
-        password1 = hash;
+        const password1 = hash;
+        console.log(password1);
       });
       var password2 = bcrypt.hash("password2", saltRounds, function(err, hash) {
-          password2 = hash;
+          const password2 = hash;
+          console.log(password1);
       });
       var password3 = bcrypt.hash("password3", saltRounds, function(err, hash) {
-        password1 = hash;
+        const password1 = hash;
+        console.log(password1);
       });
       console.log(password1, password2, password3);
       return knex('users').insert([

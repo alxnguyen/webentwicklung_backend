f60b5442d4d158fdb0483fde2acc0cd63ded0d7a
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-06 23:02:06 +0100

    test log

--- services/authService.js
+++ services/authService.js
@@ -42,7 +42,7 @@ client.on("connect", () => console.log("Successfully connected to redis"));
 
 
   async function login(email, password)    {
-    correctPassword=await checkPassword(email, password);
+    correctPassword=await checkPassword(email, password).then(() => {console.log("Done")});
     console.log("hallohallo:  "+ correctPassword)
     if(correctPassword) {
       console.log("hallohallohallo");

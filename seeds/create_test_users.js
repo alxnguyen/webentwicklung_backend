9a84ff63451017c8b99349274738c3ef47d12d93
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-06 21:40:45 +0100

    added console log

--- seeds/create_test_users.js
+++ seeds/create_test_users.js
@@ -7,15 +7,16 @@ exports.seed = function(knex) {
       // Inserts seed entries
       const saltRounds = 10;
 
-      var password1 =await bcrypt.hash("password1", saltRounds, function(err, hash) {
+      var password1 = bcrypt.hash("password1", saltRounds, function(err, hash) {
         password1 = hash;
       });
-      var password2 =await bcrypt.hash("password2", saltRounds, function(err, hash) {
+      var password2 = bcrypt.hash("password2", saltRounds, function(err, hash) {
           password2 = hash;
       });
-      var password3 =await bcrypt.hash("password3", saltRounds, function(err, hash) {
+      var password3 = bcrypt.hash("password3", saltRounds, function(err, hash) {
         password1 = hash;
       });
+      console.log(password1, password2, password3);
       return knex('users').insert([
         {id: 1, email: 'user1@email.com', password: password1},
         {id: 2, email: 'user2@email.com', password: password2},

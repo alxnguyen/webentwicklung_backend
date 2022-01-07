7467c895e699e56c739ae44027fba4560ba698af
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-07 02:03:07 +0100

    fgfdgfdgfd

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('trust proxy', 1);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 

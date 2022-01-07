ef722c13b23447b3caffb668cde738d2e3a5dfd5
alxnguyen <alexander.nguyen1509@gmail.com>
2022-01-07 02:15:17 +0100

    reversed changes

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('trust proxy', 1);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
 
 app.post("/", async (req, res) => {
   const email = req.body.email;

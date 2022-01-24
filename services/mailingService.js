62812f107f72e481b2ee391d1b7359f0deaa025b
soosaasees <nachokaese@gmx.net>
2022-01-24 01:03:23 +0100

    link gefixed

--- services/mailingService.js
+++ services/mailingService.js
@@ -11,13 +11,12 @@ let transporter=nodemailer.createTransport({
 
 module.exports = {
     async sendOptInMail(email, userID, token) {
-      let activationLink = `${process.env.BASE_URL}api/verify/${userID}/${token}`;
+      let activationLink = `${process.env.BASE_URL}/verify/${userID}/${token}`;
       let mail = {
         from: process.env.MAIL_USER,
         to: email,
         subject: "Please active your account",
         text: `To activate your account, please click this link: ${activationLink}`,
-        html: `<p>To activate your account, please click this link: <a href="${activationLink}">${activationLink}</a></p>`,
       };
       await transporter.sendMail(mail);
     },

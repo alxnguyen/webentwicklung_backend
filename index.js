5605bdda4ab5a47564359f4cc42bd0b685ad38a7
soosaasees <nachokaese@gmx.net>
2022-01-24 01:33:28 +0100

    token und userID wieder eingefuegt

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get("/map", checkLogin, async (req, res) => {
   return res.status(201).end(JSON.stringify(countries));
 })
 
-app.get("/verify", async (req, res) => {
+app.get("/verify/:userID/:token", async (req, res) => {
   console.log("an attempt was made");
   var userID=req.params.userID;
   var token=req.params.token;
